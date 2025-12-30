0f60be65639aa661aa8bf4c9afe98419f6e0caca
jayanshisinha-ui <sinhajayanshi@gmail.com>
2025-12-31 00:57:03 +0530

    Update minor2.py

--- minor2.py
+++ minor2.py
@@ -1,42 +1,34 @@
-from PIL import Image,ImageDraw
+from PIL import Image
 import math
 
-characters = "@%#*+=-:."
-
-charArray = list(characters)
+character = "@%#*+=-:."
+charArray = list(character)
 Length = len(charArray)
-val = Length/256
+val = Length / 256
 
 SFactor = 0.09
 Width = 8
 Height = 18
 
 def getChar(inputInt):
-    return charArray[math.floor(inputInt* val)]
-    
-text_file =open("generated.png","w")
+    return charArray[math.floor(inputInt * val)]
+
 image = Image.open("abhinavbindra.png")
 
-
 width, height = image.size
-print(width,height,height/width)
-image = image.resize((int(SFactor*width), int(SFactor*height*(Width/Height))),Image.NEAREST)
+image = image.resize((int(SFactor * width), int(SFactor * height * (Width / Height))), Image.NEAREST)
 width, height = image.size
 pix = image.load()
 
-GeneratedImage = Image.new('RGB',(Width*width , Height*height),color = (0,0,0))
+text_file = open("generated.txt", "w")
 
 for i in range(height):
     for j in range(width):
-        r,g,b = pix[j,i]
-        print(r) 
-        h = int(r/3 + g/3 + b/3)
-        pix[j,i]=(h,h,h)
-        text_file.write(getChar(h))
+        r, g, b = pix[j, i]
+        intensity = int((r + g + b) / 3)
+        text_file.write(getChar(intensity))
+    text_file.write("\n")
 
-    text_file.write('\n')    
-
-GeneratedImage.save('generated.png')
-
-
+text_file.close()
+print("ASCII Image Generated Successfully!")
 
